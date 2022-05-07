40288e08be4294ddd21021a154557eb6275feabc
Mahmoud Ashraf <99623614+MahmoudAshraf12@users.noreply.github.com>
2022-05-07 14:34:38 +0200

    initial

---
+++ lib/helper/data.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import '../model/categories_model.dart';
+
+List<CategoryModel> getCategories() {
+  List<CategoryModel> category = <CategoryModel>[];
+  CategoryModel categoryModel;
+
+  //1
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Development";
+  categoryModel.imageURL = "lib/assets/images/svgexport-5(16).svg";
+  categoryModel.color = Color(0xff99CAE1);
+
+  category.add(categoryModel);
+
+  //2
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Networking";
+  categoryModel.imageURL = "lib/assets/images/svgexport-5(17).svg";
+  categoryModel.color = Color(0xffE19999);
+
+  category.add(categoryModel);
+
+  //3
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Web";
+  categoryModel.imageURL = "lib/assets/images/svgexport-5(18).svg";
+  categoryModel.color = Color(0xff9EE199);
+  category.add(categoryModel);
+
+  //4
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Office-Productivity";
+  categoryModel.imageURL = "assets/dev.svg";
+  categoryModel.color = Color(0xff99CAE1);
+
+  category.add(categoryModel);
+
+  //5
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Personal-Development";
+  categoryModel.imageURL = "assets/web.svg";
+  categoryModel.color = Color(0xffE19999);
+
+  category.add(categoryModel);
+
+  //6
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = " Design";
+  categoryModel.imageURL = "assets/dev.svg";
+  categoryModel.color = Color(0xff9EE199);
+
+  category.add(categoryModel);
+
+  //7
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Marketing";
+  categoryModel.imageURL = "assets/netwrk.svg";
+  categoryModel.color = Color(0xffE19999);
+
+  category.add(categoryModel);
+
+  //8
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Language";
+  categoryModel.imageURL = "assets/web.svg";
+  categoryModel.color = Color(0xff99CAE1);
+
+  category.add(categoryModel);
+
+  //9
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Test-Prep";
+  categoryModel.imageURL = "assets/dev.svg";
+  categoryModel.color = Color(0xff9EE199);
+
+  category.add(categoryModel);
+
+  return category;
+}
